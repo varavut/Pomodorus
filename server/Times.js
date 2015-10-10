ce0a58755b62cd62443141986523394268ed7de9
varavut <varavut@outlook.com>
2015-10-10 20:54:53 +0700

    change update time strategy

--- server/Times.js
+++ server/Times.js
@@ -1,11 +1,18 @@
-Meteor.publish("times", function(){
-   return Times.find({});
+Meteor.publish("times", function () {
+    return Times.find({});
 });
 
-if(Times.find().count() == 0){
-    Times.insert({time:0});
+if (Times.find().count() == 0) {
+    console.log('create new time object');
+    Times.insert({time: 0});
 }
 
 currentTime = Times.findOne();
 
-Meteor.setInterval(function(){currentTime.time = new Date();},1000);
+console.log(currentTime);
+
+Meteor.setInterval(function () {
+    //currentTime.time = new Date();
+Times.update(currentTime._id,{$set:{time:new Date()}});
+    //console.log('currentTime updated '+ currentTime.time);
+}, 1000);
