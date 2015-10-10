bfea8307baec73f9d00a08c9c77aa09abb8a6dc2
zapkub <rungsikorn@icloud.com>
2015-10-10 16:28:31 +0700

    fix some error

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
-angular.module('pomodous',['angular-meteor']).controller('taskController',function($scope){
-    var list = [ ];//Meteor.collection('list');
+angular.module('pomodorus',['angular-meteor']).controller('taskController',function($scope){
+    $scope.list = [ ];//Meteor.collection('list');
     $scope.join = function(){
         console.log($scope.name + $scope.todo);
         $scope.list.push({name:$scope.name,todo:$scope.todo});
