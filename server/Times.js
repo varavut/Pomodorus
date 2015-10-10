62d7a325525047c74a788fc54a852ca8005227ef
varavut <varavut@outlook.com>
2015-10-10 22:08:04 +0700

    publish running state together with remaining time

--- server/Times.js
+++ server/Times.js
@@ -1,9 +1,12 @@
-Meteor.publish("times", function () {return Times.find({});});
+Meteor.publish("times", function () {
+    return Times.find({});
+});
 currentTimeId = Times.findOne()._id || Times.insert({value: 0});
 Meteor.setInterval(function () {
     var date = moment();
     var cMinutes = date.minutes();
     var nextTimeUp = cMinutes <= 25 ? 25 : cMinutes <= 30 ? 30 : cMinutes <= 55 ? 55 : 60;
     var diff = date.clone().minutes(nextTimeUp).seconds(0).diff(date);
-    Times.update(currentTimeId, {$set: {value: moment.utc(diff).format('mm:ss')}});
+    var isRunning = nextTimeUp == 25 || nextTimeUp == 55;
+    Times.update(currentTimeId, {$set: {value: moment.utc(diff).format('mm:ss'),isRunning: isRunning}});
 }, 1000);
