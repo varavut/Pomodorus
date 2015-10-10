71b78f734bb1813bdca6063a222c9ae4324e4072
zapkub <rungsikorn@icloud.com>
2015-10-10 16:18:18 +0700

    init angular app

--- client/app.js
+++ client/app.js
@@ -1,5 +1,11 @@
-var app = angular.module('pomodousTODOList',[ ]);
-
-app.controller('homeC',function($scope){
-
+angular.module('pomodous',['angular-meteor']).controller('taskController',function($scope){
+    var list = Meteor.collection('list');
+    $scope.join = function(){
+        console.log($scope.name + $scope.todo);
+        $scope.list.insert({name:$scope.name,todo:$scope.todo});
+        $scope.name = "";
+        $scope.todo = "";
+    }
 });
+
+
