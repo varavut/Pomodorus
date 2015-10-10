5bf480e436630e59ed7dbc5f8cf00b62c6f225b8
zapkub <rungsikorn@icloud.com>
2015-10-11 01:29:38 +0700

    Implement input field and display RELAX or WORK time!

--- client/app.js
+++ client/app.js
@@ -2,20 +2,35 @@
 angular.module('pomodorus',['angular-meteor']).controller('taskController',function($scope, $meteor){
     $meteor.subscribe("tasks");
     $scope.tasks = $meteor.collection(Tasks);
-    $scope.mytask = $meteor.collection(function(){
-        return Tasks.find({name:'me'});
-    });
 
+
+
+
+    $scope.user = Meteor.user();
     $scope.join = function(){
-        console.log($scope.name + $scope.todo);
-        console.log( Meteor.user());
-        var pseudoId = Math.random() * 10000000;
-        $scope.tasks.push({name:$scope.name,todo:$scope.todo,trackId:pseudoId});
+
+        if (!$scope.mytask){
+            $scope.tasks.push({name:Meteor.user().profile.name,todo:$scope.todo,trackId:Meteor.userId()});
+        }
+
     };
 
+    var getUserTask = function(){
+        $scope.mytask = $meteor.collection(function(){
+            return Tasks.find({trackId:Meteor.userId()});
+        });
+    };
+
+    if(Meteor.user()){
+
+        getUserTask();
+    }
+
+    Accounts.onLogin(function(){
+        $scope.user = Meteor.user();
+    });
     $meteor.subscribe("times");
     $scope.times = $meteor.collection(Times);
-    $scope.isRunning = $scope.times[0].isRunning;
 
 });
 
