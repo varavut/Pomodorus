565cd6fca832a4e81b1fb3c6d9af2b2d72b6e0a3
zapkub <rungsikorn@icloud.com>
2015-10-10 16:26:53 +0700

    fix some

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 angular.module('pomodous',['angular-meteor']).controller('taskController',function($scope){
-    var list = Meteor.collection('list');
+    var list = [ ];//Meteor.collection('list');
     $scope.join = function(){
         console.log($scope.name + $scope.todo);
-        $scope.list.insert({name:$scope.name,todo:$scope.todo});
+        $scope.list.push({name:$scope.name,todo:$scope.todo});
         $scope.name = "";
         $scope.todo = "";
     }
