c479af977f8cfb04666cd5c48d29a0e8cacb9a8c
zapkub <rungsikorn@icloud.com>
2015-10-10 23:14:35 +0700

    Get Timer state

--- client/app.js
+++ client/app.js
@@ -1,13 +1,16 @@
-angular.module('pomodorus',['angular-meteor']).controller('taskController',function($scope, $meteor){
+
+angular.module('pomodorus',['angular-meteor','ngRoute']).controller('taskController',function($scope, $meteor){
     $scope.tasks = $meteor.collection(Tasks);
 
     $scope.join = function(){
         console.log($scope.name + $scope.todo);
         $scope.tasks.push({name:$scope.name,todo:$scope.todo});
-    }
+    };
+
+
 
     $meteor.subscribe("times");
     $scope.times = $meteor.collection(Times);
+    $scope.isRunning = $scope.times[0].isRunning;
 });
-
 
