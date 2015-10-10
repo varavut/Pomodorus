838cda8ea3c52619fa69384cf312964357214787
varavut <varavut@outlook.com>
2015-10-10 21:19:42 +0700

    minify server side code

--- server/Times.js
+++ server/Times.js
@@ -2,17 +2,12 @@ Meteor.publish("times", function () {
     return Times.find({});
 });
 
-if (Times.find().count() == 0) {
-    console.log('create new time object');
-    Times.insert({value: 0});
-}
-
-currentTime = Times.findOne();
+currentTimeId = Times.find().count() == 0? Times.insert({value: 0}): Times.findOne()._id;
 
 Meteor.setInterval(function () {
     var date = moment();
     var currentMinutes = date.minutes();
     var nextTimeUp = currentMinutes < 25 ? 25 : currentMinutes < 30 ? 30 : currentMinutes < 55 ? 55 : 60;
     var diff = date.clone().minutes(nextTimeUp).seconds(0).diff(date);
-    Times.update(currentTime._id, {$set: {value: moment.utc(diff).format('mm:ss')}});
+    Times.update(currentTimeId, {$set: {value: moment.utc(diff).format('mm:ss')}});
 }, 1000);
