530a377473a0542a238333a8b771543d6bb5c4ed
varavut <varavut@outlook.com>
2015-10-10 21:16:43 +0700

    publish remaining time instead server time

--- server/Times.js
+++ server/Times.js
@@ -4,15 +4,15 @@ Meteor.publish("times", function () {
 
 if (Times.find().count() == 0) {
     console.log('create new time object');
-    Times.insert({time: 0});
+    Times.insert({value: 0});
 }
 
 currentTime = Times.findOne();
 
-console.log(currentTime);
-
 Meteor.setInterval(function () {
-    //currentTime.time = new Date();
-Times.update(currentTime._id,{$set:{time:new Date()}});
-    //console.log('currentTime updated '+ currentTime.time);
+    var date = moment();
+    var currentMinutes = date.minutes();
+    var nextTimeUp = currentMinutes < 25 ? 25 : currentMinutes < 30 ? 30 : currentMinutes < 55 ? 55 : 60;
+    var diff = date.clone().minutes(nextTimeUp).seconds(0).diff(date);
+    Times.update(currentTime._id, {$set: {value: moment.utc(diff).format('mm:ss')}});
 }, 1000);
