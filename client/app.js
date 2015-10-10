eefc06e1a2259785b3e7ed63f1cff8ef48086460
zapkub <rungsikorn@icloud.com>
2015-10-11 00:17:02 +0700

    Integrated Facebook login

--- client/app.js
+++ client/app.js
@@ -2,16 +2,20 @@
 angular.module('pomodorus',['angular-meteor']).controller('taskController',function($scope, $meteor){
     $meteor.subscribe("tasks");
     $scope.tasks = $meteor.collection(Tasks);
-
+    $scope.mytask = $meteor.collection(function(){
+        return Tasks.find({name:'me'});
+    });
     $scope.join = function(){
         console.log($scope.name + $scope.todo);
-        $scope.tasks.push({name:$scope.name,todo:$scope.todo});
+        var pseudoId = Math.random() * 10000000;
+        $scope.tasks.push({name:$scope.name,todo:$scope.todo,trackId:pseudoId});
     };
-
-
 
     $meteor.subscribe("times");
     $scope.times = $meteor.collection(Times);
     $scope.isRunning = $scope.times[0].isRunning;
+
+
+
 });
 
