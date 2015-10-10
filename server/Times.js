a9ca14467a443e0247926de5ff0e390322b398ff
varavut <varavut@outlook.com>
2015-10-10 21:42:48 +0700

    minify again

--- server/Times.js
+++ server/Times.js
@@ -1,13 +1,9 @@
-Meteor.publish("times", function () {
-    return Times.find({});
-});
-
-currentTimeId = Times.find().count() == 0? Times.insert({value: 0}): Times.findOne()._id;
-
+Meteor.publish("times", function () {return Times.find({});});
+currentTimeId = Times.findOne()._id || Times.insert({value: 0});
 Meteor.setInterval(function () {
     var date = moment();
-    var currentMinutes = date.minutes();
-    var nextTimeUp = currentMinutes < 25 ? 25 : currentMinutes < 30 ? 30 : currentMinutes < 55 ? 55 : 60;
+    var cMinutes = date.minutes();
+    var nextTimeUp = cMinutes <= 25 ? 25 : cMinutes <= 30 ? 30 : cMinutes <= 55 ? 55 : 60;
     var diff = date.clone().minutes(nextTimeUp).seconds(0).diff(date);
     Times.update(currentTimeId, {$set: {value: moment.utc(diff).format('mm:ss')}});
 }, 1000);
