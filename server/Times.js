ad98279386cbaf3a24fe8f66c961a1d40ebffeba
Varavut <varavut@outlook.com>
2015-10-11 07:49:25 +0700

    fix timer bug and format index.ng.html code

--- server/Times.js
+++ server/Times.js
@@ -5,7 +5,7 @@ currentTimeId = Times.find().count()?Times.findOne()._id : Times.insert({value: 
 Meteor.setInterval(function () {
     var date = moment();
     var cMinutes = date.minutes();
-    var nextTimeUp = cMinutes <= 25 ? 25 : cMinutes <= 30 ? 30 : cMinutes <= 55 ? 55 : 60;
+    var nextTimeUp = cMinutes < 25 ? 25 : cMinutes < 30 ? 30 : cMinutes < 55 ? 55 : 60;
     var diff = date.clone().minutes(nextTimeUp).seconds(0).diff(date);
     var isRunning = nextTimeUp == 25 || nextTimeUp == 55;
     Times.update(currentTimeId, {$set: {value: moment.utc(diff).format('mm:ss'),isRunning: isRunning}});
