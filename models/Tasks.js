c0a56eb3f5743cf2eef8917d5f4c8957f1137715
Mike Wheaton <mike@mikewheaton.ca>
2015-10-11 15:41:09 +0700

    Reduced to 97 lines, with no lines exceeding 80 characters.

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,12 +1,7 @@
 Tasks =  new Mongo.Collection('tasks');
-
 Tasks.allow({
-    insert: function (userId, swot) {
-        return true;
-    },
-    update: function (userId, task, fields, modifier) {
-        return true;
-    },
+    insert: function (userId, swot) { return true; },
+    update: function (userId, task, fields, modifier) { return true; },
     remove: function (userId, swot) {
         if(Meteor.isServer) return true; return false;
     }
--- server/Times.js
+++ server/Times.js
@@ -1,12 +1,12 @@
-Meteor.publish("times", function () {
-    return Times.find({});
-});
-currentTimeId = Times.find().count() ? Times.findOne()._id : Times.insert({value: 0});
+Meteor.publish("times", function () { return Times.find({}); });
+currentTimeId =
+  Times.find().count() ? Times.findOne()._id : Times.insert({value: 0});
 var needToClearTask = true;
 Meteor.setInterval(function () {
     var date = moment();
     var cMinutes = date.minutes();
-    var nextTimeUp = cMinutes < 25 ? 25 : cMinutes < 30 ? 30 : cMinutes < 55 ? 55 : 60;
+    var nextTimeUp =
+      cMinutes < 25 ? 25 : cMinutes < 30 ? 30 : cMinutes < 55 ? 55 : 60;
     var diff = date.clone().minutes(nextTimeUp).seconds(0).diff(date);
     var isRunning = nextTimeUp == 25 || nextTimeUp == 55;
     if (isRunning && needToClearTask) {
@@ -15,5 +15,6 @@ Meteor.setInterval(function () {
     } else if (!isRunning) {
         needToClearTask = true;
     }
-    Times.update(currentTimeId, {$set: {value: moment.utc(diff).format('mm:ss'), isRunning: isRunning}});
+    Times.update(currentTimeId, {$set: {value: moment.utc(diff).format('mm:ss'),
+    isRunning: isRunning}});
 }, 1000);
