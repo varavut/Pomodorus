4731371595aa0b0c617eb6363a46c4ce135d0aec
Varavut <varavut@outlook.com>
2015-10-11 10:58:22 +0700

    remove unused code

--- client/app.js
+++ client/app.js
@@ -1,14 +1,12 @@
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+});
+
 angular.module('pomodorus', ['angular-meteor']).controller('taskController', function ($scope, $meteor, $rootScope) {
     $meteor.subscribe("tasks");
     $scope.tasks = $meteor.collection(Tasks);
     $scope.join = function () {
         if ($scope.mytask.length == 0) {
-            var name = 'unknow';
-            if ($rootScope.currentUser.profile) {
-                name = $rootScope.currentUser.profile.name;
-            } else {
-                name = $rootScope.currentUser.emails[0].address;
-            }
             if ($scope.todo) {
                 $scope.tasks.push(
                     {
